8d20235d3ca3274204eb72d363a77b082cb33681
Michael Thomas <Michaelt293@gmail.com>
2019-05-10 21:46:10 +1000

    Added clap and work email

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+extern crate clap;
 #[macro_use]
 extern crate kinesis_layout;
 extern crate rayon;
 
+use std::convert::AsRef;
 use std::fs::File;
 use std::io::Write;
 
+use clap::{App, Arg};
 use rayon::prelude::*;
 
 use kinesis_layout::configure::*;
@@ -16,13 +19,27 @@ fn main() {
     use Modifier::*;
     use NonModifier::*;
 
-    fn layout(system: System) -> Layout {
+    let email = "Michaelt293@gmail.com";
+
+    let matches = App::new("My keyboard layouts")
+        .version("0.1")
+        .author(format!("Michael T. <{}>", email).as_ref())
+        .about("Does awesome things")
+        .arg(
+            Arg::with_name("email")
+                .long("work-email")
+                .required(true)
+                .takes_value(true),
+        )
+        .get_matches();
+
+    fn layout(system: System, email: &str) -> Layout {
         Configure::new()
             .with_remappings(colemak())
             .set_system(system)
             .with_macro(
                 Shortcut::keypad_off(btreeset! {LeftAlt}, K),
-                MacroBuilder::from_string("Michaelt293@gmail.com").make(),
+                MacroBuilder::from_string(email).make(),
             )
             .with_macro(
                 Shortcut::keypad_off(btreeset! {LeftAlt}, S),
@@ -65,15 +82,22 @@ fn main() {
             .expect(&("Unable to create file: ".to_owned() + file_name))
     };
 
-    fn write_file(file_name: &str, system: System) -> () {
+    fn write_file(file_name: &str, system: System, email: &str) -> () {
         create_file(file_name)
-            .write_all(format!("{}", layout(system)).as_bytes())
+            .write_all(format!("{}", layout(system, email)).as_bytes())
             .expect("Unable to write data")
     };
 
-    let system_file_names = [(w_colemak, System::PC), (m_colemak, System::Mac)];
+    let system_file_names = [
+        (
+            w_colemak,
+            System::PC,
+            matches.value_of("email").expect("work email is required"),
+        ),
+        (m_colemak, System::Mac, email),
+    ];
 
     system_file_names
         .par_iter()
-        .for_each(|sys_file_name| write_file(sys_file_name.0, sys_file_name.1));
+        .for_each(|sys_file_name| write_file(sys_file_name.0, sys_file_name.1, sys_file_name.2));
 }
